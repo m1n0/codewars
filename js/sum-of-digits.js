0794962d64dda3bb4e98d68abe77f8791bd35c4f
m1n0 <mino.lukac@gmail.com>
2016-07-18 10:55:18 +0200

    JS Sum of digits - wicked math based community solution

--- js/sum-of-digits.js
+++ js/sum-of-digits.js
@@ -1,17 +1,19 @@
 // https://www.codewars.com/kata/541c8630095125aba6000c00
 function digital_root(n) {
   var digits = (""+n).split("");
-
   var sum = digits.reduce(function(s, d) {
-    return s + d;
+    return parseInt(s) + parseInt(d);
   });
-
   var sumDigits = (""+sum).split("");
 
   if (sumDigits.length == 1) {
-    return sum;
+    return parseInt(sum);
   }
   else {
     return digital_root(sum);
   }
+}
+
+function digital_root_wicked(n) {
+  return (n - 1) % 9 + 1;
 }
