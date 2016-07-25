707526b6ee6b85175a80f6e4851804e9a7b795fe
m1n0 <mino.lukac@gmail.com>
2016-07-26 00:11:46 +0200

    PY Names list - 3rd iteration - using formatted string to return instead of concetation

--- python/names-list.py
+++ python/names-list.py
@@ -3,4 +3,5 @@ def namelist(names):
     l = len(names)
     if (l == 0): return ''
     elif (l == 1): return names[0]['name']
-    else: return ', '.join([n['name'] for n in names[:-1]]) + ' & ' + names[-1]['name']
+    else: return '{} & {}'.format(', '.join([n['name'] for n in names[:-1]]),
+                                names[-1]['name'])
