751a05a007eb3c3d4ee9b4738581de77af7bb5fe
m1n0 <mino.lukac@gmail.com>
2016-07-28 16:16:54 +0200

    JS Functional calculations - quick and dirty with eval

---
+++ js/calc-func.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/calculating-with-functions
+function zero(f) {if (typeof f === 'undefined') return 0; else return eval("0 " + f)}
+function one(f) {if (typeof f === 'undefined') return 1; else return eval("1 " + f)}
+function two(f) {if (typeof f === 'undefined') return 2; else return eval("2 " + f)}
+function three(f) {if (typeof f === 'undefined') return 3; else return eval("3 " + f)}
+function four(f) {if (typeof f === 'undefined') return 4; else return eval("4 " + f)}
+function five(f) {if (typeof f === 'undefined') return 5; else return eval("5 " + f)}
+function six(f) {if (typeof f === 'undefined') return 6; else return eval("6 " + f)}
+function seven(f) {if (typeof f === 'undefined') return 7; else return eval("7 " + f)}
+function eight(f) {if (typeof f === 'undefined') return 8; else return eval("8 " + f)}
+function nine(f) {if (typeof f === 'undefined') return 9; else return eval("9 " + f)}
+
+function plus(n) {return "+ " + n}
+function minus(n) {return "- " + n}
+function times(n) {return "* " + n}
+function dividedBy(n) {return "/ " + n}
