e1babb28c3374b5ed68011621bf1bdcf37c80d8a
m1n0 <mino.lukac@gmail.com>
2016-08-22 14:48:12 +0200

    PY Delete Occurrences much simpler solution

--- python/delete-occurrences.py
+++ python/delete-occurrences.py
@@ -1,14 +1,6 @@
 # https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/python
 def delete_nth(order,max_e):
-    elements = {}
     res = []
     for n in order:
-        if n in elements:
-            if elements[n] < max_e:
-                elements[n] += 1
-                res.append(n)
-        else:
-            elements[n] = 1
-            res.append(n)
-
+        if res.count(n) < max_e: res.append(n)
     return res
