d6dbf4b443f35d223a89ea9d3e557d9641ff561e
m1n0 <mino.lukac@gmail.com>
2017-06-07 18:23:15 +0200

    PY having fun with strings - added more "traditional" approach

--- python/mumbling.py
+++ python/mumbling.py
@@ -2,4 +2,11 @@
 def accum(s):
     return "-".join([c.upper() + c.lower() * i for i, c in enumerate(s)])
 
+def accum_traditional(s):
+    groups = []
+    for i, c in enumerate(s):
+        groups.append((c + c * i).capitalize())
+    return "-".join(groups)
+
 print(accum("ZpglnRxqenU"))
+print(accum_traditional("ZpglnRxqenU"))
