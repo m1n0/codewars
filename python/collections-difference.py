7aacb67e7c33470a70df6d810c7a4d31257a500d
m1n0 <mino.lukac@gmail.com>
2017-06-16 08:51:32 +0200

    PY Collections difference - fancy binary solution

--- python/collections-difference.py
+++ python/collections-difference.py
@@ -1,3 +1,7 @@
 # https://www.codewars.com/kata/difference-between-two-collections
 def diff(a, b):
     return sorted(list(set([c for c in a if c not in b] + [c for c in b if c not in a])))
+
+
+def diff_binary(a, b):
+    return sorted(set(a) ^ set(b))
